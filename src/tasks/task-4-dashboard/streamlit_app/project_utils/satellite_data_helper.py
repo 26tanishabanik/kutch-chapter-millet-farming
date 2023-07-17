eaafc4fefd72a5383b0b8a4ed2e4aa693b31e03b
Chancy Shah <72352809+chancyshah@users.noreply.github.com>
2023-07-17 17:03:31 +0530

    Update satellite_data_helper.py

--- src/tasks/task-4-dashboard/streamlit_app/project_utils/satellite_data_helper.py
+++ src/tasks/task-4-dashboard/streamlit_app/project_utils/satellite_data_helper.py
@@ -165,34 +165,34 @@ def fetch_satellite_data(start_date, end_date, aoi_roi):
       'grant_type': 'authorization_code'
     }
     refresh_token = None
-    try:
+  try:
         response = urllib2.urlopen('https://accounts.google.com/o/oauth2/token',
                             urllib.urlencode(token_request_params)).read()
         tokens = json.loads(response)
         refresh_token = tokens['refresh_token']
-    except urllib2.HTTPError, e:
+  except urllib2.HTTPError, e:
         raise Exception('Problem requesting tokens.  Please try again.  %s %s' %
                     (e, e.read()))
 
     ### Write refresh token to filesystem for later use
-    credentials_path = OAuthInfo.credentials_path()
-    dirname = os.path.dirname(credentials_path)
-    try:
+  credentials_path = OAuthInfo.credentials_path()
+  dirname = os.path.dirname(credentials_path)
+  try:
         os.makedirs(dirname)
-    except OSError, e:
+  except OSError, e:
         if e.errno != errno.EEXIST:
             raise Exception('Error creating %s: %s' % (dirname, e))
 
-    json.dump({'refresh_token': refresh_token}, open(credentials_path, 'w'))
+  json.dump({'refresh_token': refresh_token}, open(credentials_path, 'w'))
 
-    print '\nSuccessfully saved authorization to %s' % credentials_path
+  print '\nSuccessfully saved authorization to %s' % credentials_path
     
 # Try to authenticate to Earth Engine.
-try:
-  ee.Initialize()
-  print '\nSuccessfully authenticated to Earth Engine!'
-except:
-  print '\nOops. Something went wrong!'
+  try:
+    ee.Initialize()
+    print '\nSuccessfully authenticated to Earth Engine!'
+  except:
+    print '\nOops. Something went wrong!'
 
   
   poly = shapely.geometry.Polygon(aoi_roi[0])
