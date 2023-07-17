d67e6258cb2e9545686cac320fded2d1ec11efd7
Chancy Shah <72352809+chancyshah@users.noreply.github.com>
2023-07-17 13:27:49 +0530

    Update About_Project.py

--- src/tasks/task-4-dashboard/streamlit_app/About_Project.py
+++ src/tasks/task-4-dashboard/streamlit_app/About_Project.py
@@ -4,7 +4,10 @@ from datetime import date
 import os
 import glob as glob
 from project_utils.page_layout_helper import set_page_settings, get_page_title, main_header
+import ee
 
+ee.Authenticate()
+ee.Initialize()
 def active_contributors():
   ACTIVE_CONTRIBUTORS_PAGE_CHAPTERLEAD='''
 | Chapter Name | Lead Name |
