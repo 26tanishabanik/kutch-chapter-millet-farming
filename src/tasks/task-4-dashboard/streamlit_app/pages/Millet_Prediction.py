c5c2601761655df577d9b4cc30fce53920e11d09
Chancy Shah <72352809+chancyshah@users.noreply.github.com>
2023-07-18 12:41:11 +0530

    Update Millet_Prediction.py

--- src/tasks/task-4-dashboard/streamlit_app/pages/Millet_Prediction.py
+++ src/tasks/task-4-dashboard/streamlit_app/pages/Millet_Prediction.py
@@ -164,7 +164,7 @@ def main():
   with st.form("my_form"):
     m = folium.Map()
     Draw(export = False, draw_options={ "polygon" : False, "polyline" : False, "circle" : False, "marker" : False, "circlemarker" : False},edit_options=False).add_to(m)
-    polygon_coordinates = st_folium(m, width=800, height=500)
+    polygon_coordinates = st_folium(m, width=800, height=1000)
     
     option = st.selectbox('Please select the month of sowing : ', ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'))
 
